63ce9ef82166d2c49882f4e60336cd50a087f383
Gabriel Jean <mpuhwegabriel@gmail.com>
2026-01-29 21:24:21 +0200

    correct deployement

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/personal-portfolio/',   // 👈 important for GitHub Pages (repo site)
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
